185a22a37e2f0077e5fccab44c58c30d91a917cd
Rahul Yadav <ryadav@beesolvertechnology.com>
2020-04-02 17:52:46 +0530

    small changes

--- AttendanceApp/ViewModels/LoginViewModel.cs
+++ AttendanceApp/ViewModels/LoginViewModel.cs
@@ -119,13 +119,17 @@ namespace AttendanceApp.ViewModels
 
                         ImageBase64 = objUser.src;
                         ImageType = orgDetails.type;
-                        LangType = JsonConvert.DeserializeObject<Language>(orgDetails.name);
+                      //  LangType = JsonConvert.DeserializeObject<Language>(orgDetails.name);
+                        
+                            Device.BeginInvokeOnMainThread(async () =>
+                            {
+                                await MaterialDialog.Instance.SnackbarAsync(message: Resx.AppResources.pleaseCheckYourNetworkConnection,
+                                   msDuration: MaterialSnackbar.DurationLong);
+                            });
+                        
+                      
 
-                        Device.BeginInvokeOnMainThread(async () =>
-                        {
-                            await MaterialDialog.Instance.SnackbarAsync(message: Resx.AppResources.pleaseCheckYourNetworkConnection,
-                               msDuration: MaterialSnackbar.DurationLong);
-                        });
+
                         return;
 
                     }
