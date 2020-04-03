8631d0d18c6c6237e315ceb1145ecb463f53050d
viveklpu2011 <viveklpu2011@gmail.com>
2020-04-03 09:14:56 +0530

    added file

---
+++ AttendanceApp/Database/DBModel/CheckinCheckoutDBModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SQLite;
+
+namespace AttendanceApp.Database.DBModel
+{
+    
+    public class ClsDBLocationData 
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string description { get; set; }
+        public string mapLocation { get; set; }
+        public double lat { get; set; }
+        public double lng { get; set; }
+        public double radius { get; set; }
+    }
+}
--- AttendanceApp/Database/DBModel/LoginDBModel.cs
+++ AttendanceApp/Database/DBModel/LoginDBModel.cs
@@ -9,6 +9,7 @@ namespace AttendanceApp.Database
     }
     public class LoginDBModel
     {
+        [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
         public string UserName { get; set; }
         public string Password { get; set; }
